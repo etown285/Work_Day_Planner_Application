f0640c14a36d9fe37529e28ee4e8ffde5bf8090c
etown285 <78112738+etown285@users.noreply.github.com>
2021-03-13 13:52:09 -0500

    organized JS

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     var currentHour = currentDate.hours()
     console.log(currentHour)
 
+//Array creating hours to match for the storage and row color 
     var hours =[
         "9", 
         "10",
@@ -23,39 +24,17 @@ $(document).ready(function() {
     ] 
 
 
-
     //event listener that stores text content into the the text area
     
     $(".saveBtn").on("click", function() {
-    // window.localStorage.setItem("9",( $("#9").val() ))
-    // window.localStorage.setItem("10",($("#hour2").val()))
-    // window.localStorage.setItem("11",($("#hour3").val()))
-    // window.localStorage.setItem("12",($("#hour4").val()))
-    // window.localStorage.setItem("1",($("#hour5").val()))
-    // window.localStorage.setItem("2",($("#hour6").val()))
-    // window.localStorage.setItem("3",($("#hour7").val()))
-    // window.localStorage.setItem("4",($("#hour8").val()))
-    // window.localStorage.setItem("5",($("#hour9").val()))
+
 
         for (i = 0; i < hours.length; i++) {
             var id = hours[i];
             window.localStorage.setItem(id,( $("#" + id).val() ))
         }
     })
-
-    //create local storage that saves what the user has entered
-    /*
-    $("#9").val(localStorage.getItem("9"));
-    $("#hour2").append(localStorage.getItem($("10")));
-    $("#hour3").append(localStorage.getItem($("11")));
-    $("#hour4").append(localStorage.getItem($("12")));
-    $("#hour5").append(localStorage.getItem($("1")));
-    $("#hour6").append(localStorage.getItem($("2")));
-    $("#hour7").append(localStorage.getItem($("3")));
-    $("#hour7").append(localStorage.getItem($("4")));
-    $("#hour7").append(localStorage.getItem($("5")));
-    */
-
+    
     //create local storage that saves what the user has entered and converted to for loop.
 
     function SetHoursOnLoad(){
